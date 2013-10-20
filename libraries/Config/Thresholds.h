f8c620f0700a03017822249689d1dc475bfe5f50
Anthony Day <day1118@gmail.com>
2013-10-20 13:16:29 +1100

    Updated code to use defines over constants
    Removed constants from wall follow code and replaced with #define for
    ease of editing & reading.

--- libraries/Config/Thresholds.h
+++ libraries/Config/Thresholds.h
@@ -3,10 +3,19 @@
 #define IRFL_SIDE_Thresh		200
 #define IRFR_SIDE_Thresh		180
 
-#define IRBL_BACK_Thresh		200
-#define IRBR_BACK_Thresh		200
-#define IRBL_SIDE_Thresh		150
-#define IRBR_SIDE_Thresh		300
+#define IRBL_FRONT_CLOSE_Thresh 	350
+#define IRBL_FRONT_FAR_Thresh		100		// Coul be 150
+#define IRBR_FRONT_CLOSE_Thresh 	200
+#define IRBR_FRONT_FAR_Thresh		100
+#define IRBL_SIDE_CLOSE_Thresh		250
+#define IRBL_SIDE_FAR_Thresh		100
+#define IRBR_SIDE_CLOSE_Thresh		300
+#define IRBR_SIDE_FAR_Thresh		100
+
+#define IRBL_BACK_Thresh		IRBL_FRONT_FAR_Thresh /// These should be de
+#define IRBR_BACK_Thresh		IRBR_FRONT_FAR_Thresh
+#define IRBL_SIDE_Thresh		IRBL_SIDE_FAR_Thresh /// These should be de
+#define IRBR_SIDE_Thresh		IRBR_SIDE_FAR_Thresh
 
 #define BALL_IR_Thresh          150
 #define BALL_RED_Thresh         100
@@ -32,3 +41,5 @@
 #define CAMERA__VALUE_Thresh		800
 #define CAMERA_MIN_WIDTH			10
 #define CAMERA_CENTERED_WIDTH		40
+
+#define CORNER_STALL_DETECT_TIME	5000
